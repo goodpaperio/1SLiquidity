a3846cc93e7d31927eaeea82a6929565cdf64a68
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2025-10-22 15:52:34 +0500

    chore: add DAI

--- keeper/src/tests/liquidity-analysis.ts
+++ keeper/src/tests/liquidity-analysis.ts
@@ -40,6 +40,7 @@ const BASE_TOKENS = {
   USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // Ethereum USDC
   USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7', // Ethereum USDT
   WBTC: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', // Ethereum WBTC
+  DAI: '0x6B175474E89094C44Da98b954EedeAC495271d0F', // Ethereum DAI
 }
 
 // Function to check if a token is an ERC20 token
@@ -898,6 +899,7 @@ async function transformToColumnFormat(
     '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
     '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
     '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
+    '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
   ])
 
   // Group by token pair (tokenA + tokenB combination)
